41a3c15d49ebda690fb6920c4b72c6c2056a2e40
Ramesh <rameshkumarkdm1993@gmail.com>
2022-10-01 00:58:41 +0530

    Add bid service bus trigger

---
+++ FSE-E-AUCTION/ProductAddBidAzureFunction/Startup.cs
@@ -0,0 +1,25 @@
+﻿
+using DeleteProductOrchestrator;
+using E_auction.Business.Contract.CommandHandlers;
+using E_auction.Business.Directors;
+using E_auction.Business.Handlers.CommandsHandlers;
+using E_auction.Business.Repositories;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+namespace DeleteProductOrchestrator
+{
+    public class Startup : FunctionsStartup
+    {
+        [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddTransient<ISaveBuyerCommandHandler, SaveBuyerCommandHandler>();
+        }
+    }
+}
