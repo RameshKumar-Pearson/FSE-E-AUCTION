d64814307053c8903691e448661729ecddd4c69b
Ramesh <rameshkumarkdm1993@gmail.com>
2022-10-02 23:03:19 +0530

    Update nuget package

---
+++ FSE-E-AUCTION/eauctionbideventhandler/Startup.cs
@@ -0,0 +1,22 @@
+﻿
+using E_auction.Business.Contract.CommandHandlers;
+using E_auction.Business.Handlers.CommandsHandlers;
+using Eauctionbideventhandler;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+namespace Eauctionbideventhandler
+{
+    public class Startup : FunctionsStartup
+    {
+        [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddTransient<ISaveBuyerCommandHandler, SaveBuyerCommandHandler>();
+        }
+    }
+}
