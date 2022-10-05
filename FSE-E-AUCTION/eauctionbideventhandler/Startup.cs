481dfba72c4ac145c8315c20362f73567ccd0ec7
Ramesh <rameshkumarkdm1993@gmail.com>
2022-10-05 22:12:59 +0530

    Commit

--- FSE-E-AUCTION/eauctionbideventhandler/Startup.cs
+++ FSE-E-AUCTION/eauctionbideventhandler/Startup.cs
@@ -3,6 +3,7 @@ using E_auction.Business.Contract.CommandHandlers;
 using E_auction.Business.Handlers.CommandsHandlers;
 using Eauctionbideventhandler;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -13,6 +14,13 @@ namespace Eauctionbideventhandler
 {
     public class Startup : FunctionsStartup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
         public override void Configure(IFunctionsHostBuilder builder)
         {
