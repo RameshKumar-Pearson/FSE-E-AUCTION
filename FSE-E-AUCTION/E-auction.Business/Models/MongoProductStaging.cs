dd1bb2b43d1d4f82a86ed061c27fbc48157978fa
Ramesh <rameshkumarkdm1993@gmail.com>
2022-10-08 20:40:10 +0530

    Update

---
+++ FSE-E-AUCTION/E-auction.Business/Models/MongoProductStaging.cs
@@ -0,0 +1,49 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace E_auction.Business.Models
+{
+   public class MongoProductStaging
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+     
+        /// <summary>
+        /// Gets (or) Sets the name of the product
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets (or) Sets the short-description of the product
+        /// </summary>
+        public string ShortDescription { get; set; }
+
+        /// <summary>
+        /// Gets (or) Sets the detailed-description of the product
+        /// </summary>
+        public string DetailedDescription { get; set; }
+
+        /// <summary>
+        /// Gets (or) Sets the category of the product
+        /// </summary>
+        public string Category { get; set; }
+
+        /// <summary>
+        /// Gets (or) Sets the starting price of the product
+        /// </summary>
+        public int StartingPrice { get; set; }
+
+        /// <summary>
+        /// Gets (or) Sets the bid end date
+        /// </summary>
+        public DateTime BidEndDate { get; set; }
+
+        /// <summary>
+        /// Gets (or) Sets the seller Id 
+        /// </summary>
+        public string SellerId { get; set; }
+    }
+}
--- FSE-E-AUCTION/E-auction.Business/Repositories/ProductDeleteRepository.cs
+++ FSE-E-AUCTION/E-auction.Business/Repositories/ProductDeleteRepository.cs
@@ -16,7 +16,7 @@ namespace E_auction.Business.Repositories
     /// </summary>
     public class ProductDeleteRepository : IProductDeleteRepository
     {
-        private readonly IMongoCollection<MongoProduct> _productCollection;
+        private readonly IMongoCollection<MongoProductStaging> _productCollection;
         private IMongoDatabase mongoDatabase;
 
         /// <summary>
@@ -26,21 +26,21 @@ namespace E_auction.Business.Repositories
         {
             var client = new MongoClient("mongodb://fseeauction:6XXU0qrKrijEPmQARM2EHcQ6y926M6R3bONSYHlqaVl0VEzit65prvz275XQ4YxcI61zCYTxWWFlXSR6Yh0GEg==@fseeauction.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@fseeauction@");
               mongoDatabase = client.GetDatabase("e-auction");
-            _productCollection = mongoDatabase.GetCollection<MongoProduct>("product_details");
+            _productCollection = mongoDatabase.GetCollection<MongoProductStaging>("product_details");
         }
 
         ///<inheritdoc/>
         public async Task<bool> DeleteProductAsync(string ProductId)
         {
             bool isDeleted = false;
-            var filter = Builders<MongoProduct>.Filter.Eq(product => product.Id, ProductId.ToString());
+
+            var filter = Builders<MongoProductStaging>.Filter.Eq(product => product.Id, ObjectId.Parse(ProductId));
             var productDeleteResult = await _productCollection.DeleteOneAsync(filter);
 
             if (productDeleteResult.DeletedCount == 1)
             {
                 isDeleted = true;
             }
-
             return isDeleted;
         }
     }
